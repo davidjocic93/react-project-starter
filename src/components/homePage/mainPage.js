dec6d6d731a5281a2d389b4810715bcfbf681d81
BIT Student <classroom@bgit.rs>
2017-11-21 17:45:06 +0100

    header_main_page

---
+++ src/components/homePage/mainPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "HeaderPage";
+
+
+class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+        
+
+    }
+    render (){
+        return (
+            <Header/>
+        );
+    }
+};
+
+export default MainPage;
