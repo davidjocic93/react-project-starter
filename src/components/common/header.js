84f390d7247181cda8ad359d9435eaaccb262576
Marija Nikolic <m.nikolic1144@gmail.com>
2017-11-23 23:05:27 +0100

    modal form styled and profile style updated

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -14,7 +14,7 @@ const Header = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg ">
-                <Link className="navbar-brand" to="/">BitBook</Link>
+                <Link className="navbar-brand" id="logo" to="/">BitBook</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
                     <span className="navbar-toggler-icon"></span>
